b42150be5b36d459911cad5f4fb4063a8cb70f15
Lucas Burlingham <lucas.burlingham@icloud.com>
2022-03-15 00:33:54 -0400

    Set lang as session variable if specified

--- steps/1_disks.php
+++ steps/1_disks.php
@@ -4,9 +4,14 @@ require_once '../config.php';
 
 // Goal: Get input from the user about their system with scraped options from basic system info and generate a executable .sh file to be run on the system to install the system.
 
-// Welcome page by LPB on Mon Mar 14 02:02:27 PM EDT 2022
+session_start();
 
-// $str = shell_exec("sudo parted -l");
+if(isset($_GET['lang'])){
+	$_SESSION['lang'] = $_GET['lang'];
+}
+
+print_r($_SESSION);
+
 
 ?>
 
@@ -26,7 +31,6 @@ require_once '../config.php';
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
 	<script src="../main.js"></script>
 	<link rel="stylesheet" href="../main.css">
-	<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.27.0/components/prism-shell-session.min.js"></script> -->
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.27.0/prism.min.js"></script>
 
 </head>
@@ -43,33 +47,35 @@ require_once '../config.php';
 			<div class="col-lg-2 col-md-1 col-sm-1">
 			</div>
 			<div class="col-lg-8 col-md-8 col-sm-10">
-				<pre class="bg-dark p-4"><?php echo shell_exec("sudo df -h | grep -v 'tmpfs\|loop'"); ?></pre>
+				<pre
+					class="bg-dark p-4"><?php echo shell_exec("df -h --block-size 1M | grep -v 'tmpfs\|loop'"); ?></pre>
 			</div>
 			<div class="col-lg-2 col-md-2 col-sm-1">
 			</div>
 		</div>
-		<div class="row">
-			<div class="col-lg-2 col-md-1 col-sm-1">
-			</div>
-			<div class="col-lg-8 col-md-8 col-sm-10">
-				<p class="text-small text-center">
-					Please select the options from the dropdowns below to create partitions for your system.
-				</p>
-				<div class="accordion" id="partition_selections">
-					<div class="accordion-item bg-dark">
-						<h2 class="accordion-header bg-dark" id="efi_heading">
-							<button class="accordion-button bg-dark text-light" type="button" data-bs-toggle="collapse"
-								data-bs-target="#efi_collapse" aria-expanded="true" aria-controls="efi_collapse">
-								EFI Partition
-							</button>
-						</h2>
-						<div id="efi_collapse" class="accordion-collapse collapse show" aria-labelledby="efi_heading"
-							data-bs-parent="#partition_selections">
-							<div class="accordion-body">
-								<form method="GET">
+		<form action="/steps/2_packages.php" method="get">
+			<div class="row">
+				<div class="col-lg-2 col-md-1 col-sm-1">
+				</div>
+				<div class="col-lg-8 col-md-8 col-sm-10">
+					<p class="text-small text-center">
+						Please select the options from the dropdowns below to create partitions for your system.
+					</p>
+					<div class="accordion-flush" id="partition_selections">
+						<div class="accordion-item bg-dark border border-secondary">
+							<h2 class="accordion-header" id="efi_heading">
+								<button class="accordion-button bg-dark text-white" type="button"
+									data-bs-toggle="collapse" data-bs-target="#efi_collapse" aria-expanded="true"
+									aria-controls="efi_collapse">
+									EFI Partition
+								</button>
+							</h2>
+							<div id="efi_collapse" class="accordion-collapse collapse show"
+								aria-labelledby="efi_heading" data-bs-parent="#partition_selections">
+								<div class="accordion-body">
 									<div class="form-group">
-										<label for="partition_type">Root Drive Selection</label>
-										<select class="form-control" id="drive_path" name="drive_path">
+										<label for="efi_drive_path">EFI Drive Selection</label>
+										<select class="form-control" id="efi_drive_path" name="efi_drive_path">
 											<?php
 												// Grab all the drives from the system
 												// get SATA drives
@@ -88,8 +94,54 @@ require_once '../config.php';
 										</select>
 									</div>
 									<div class="form-group">
-										<label for="partition_type">Partition Type</label>
-										<select class="form-control" id="partition_type" name="partition_type">
+										<label for="efi_partition_size">Partition Size (MB)</label>
+										<input type="number" class="form-control" id="efi_partition_size"
+											name="efi_partition_size"
+											placeholder="Enter partition size in MB, blank for rest of drive">
+									</div>
+									<div class="form-group">
+										<label for="root_mout_point">Partition Mount Point</label>
+										<input type="text" class="form-control" id="root_mout_point"
+											name="root_mout_point" placeholder="/mnt/efi" value="/mnt/efi">
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="accordion-item bg-dark border border-secondary">
+							<h2 class="accordion-header bg-dark" id="root_heading">
+								<button class="accordion-button bg-dark text-white collapsed" type="button"
+									data-bs-toggle="collapse" data-bs-target="#data_collapse" aria-expanded="false"
+									aria-controls="data_collapse">
+									Data Partition
+								</button>
+							</h2>
+							<div id="data_collapse" class="accordion-collapse collapse border border-light"
+								aria-labelledby="root_heading" data-bs-parent="#partition_selections">
+								<div class="accordion-body">
+									<div class="form-group">
+										<label for="root_drive_path">Root Drive Selection</label>
+										<select class="form-control" id="root_drive_path" name="root_drive_path">
+											<?php
+												// Grab all the drives from the system
+												// get SATA drives
+												foreach (glob("/dev/sd*") as $filename) {
+													echo "<option value='$filename'>$filename</option>";
+												}
+												// get NVMe drives
+												foreach (glob("/dev/nvme*") as $filename) {
+													echo "<option value='$filename'>$filename</option>";
+												}
+												// get eMMC drives
+												foreach (glob("/dev/mmcblk*") as $filename) {
+													echo "<option value='$filename'>$filename</option>";
+												}
+											?>
+										</select>
+									</div>
+									<div class="form-group">
+										<label for="root_partition_type">Partition Type</label>
+										<select class="form-control" id="root_partition_type"
+											name="root_partition_type">
 											<option value="ext4">ext4</option>
 											<option value="ext3">ext3</option>
 											<option value="ext2">ext2</option>
@@ -98,59 +150,37 @@ require_once '../config.php';
 										</select>
 									</div>
 									<div class="form-group">
-										<label for="partition_size">Partition Size</label>
-										<input type="text" class="form-control" id="partition_size"
-											name="partition_size" placeholder="Enter partition size in MB">
+										<label for="root_partition_size">Partition Size (MB)</label>
+										<input type="number" class="form-control" id="root_partition_size"
+											name="root_partition_size" placeholder="100">
 									</div>
 									<div class="form-group">
-										<label for="partition_mount_point">Partition Mount Point</label>
-										<input type="text" class="form-control" id="partition_mount_point"
-											name="partition_mount_point" placeholder="/mnt" value="/mnt">
+										<label for="efi_mount_point">Partition Mount Point</label>
+										<input type="text" class="form-control" id="efi_mount_point"
+											name="efi_mount_point" placeholder="/mnt" value="/mnt">
 									</div>
-									<button type="submit" class="btn btn-primary">Submit</button>
-								</form>
+								</div>
 							</div>
 						</div>
 					</div>
-					<div class="accordion-item">
-						<h2 class="accordion-header" id="root_heading">
-							<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-								data-bs-target="#data_collapse" aria-expanded="false" aria-controls="data_collapse">
-								Data Partition
+
+					<div class="row">
+						<div class="col-5"></div>
+						<div class="col-2">
+							<button class="btn btn-primary font-major-mono-display mt-3" type="submit"
+								onclick="fade_out(1);">Packages
+								<i class="fa fa-arrow-right" aria-hidden="true"></i>
 							</button>
-						</h2>
-						<div id="data_collapse" class="accordion-collapse collapse" aria-labelledby="root_heading"
-							data-bs-parent="#partition_selections">
-							<div class="accordion-body">
-								<strong>This is the second item's accordion body.</strong> It is hidden by default,
-								until the collapse plugin adds the appropriate classes that we use to style each
-								element. These classes control the overall appearance, as well as the showing and hiding
-								via CSS transitions. You can modify any of this with custom CSS or overriding our
-								default variables. It's also worth noting that just about any HTML can go within the
-								<code>.accordion-body</code>, though the transition does limit overflow.
-							</div>
 						</div>
+						<div class="col-5"></div>
 					</div>
 				</div>
-
-			</div>
-			<div class="col-lg-2 col-md-2 col-sm-1">
-			</div>
-
-
-		</div>
-		<div class="row">
-			<div class="col-5"></div>
-			<div class="col-2">
-				<button class="btn btn-primary font-major-mono-display w-100" onclick="fade_out(1);">Packages
-					<i class="fa fa-arrow-right" aria-hidden="true"></i>
-				</button>
-			</div>
-			<div class="col-5"></div>
+		</form>
+		<div class="col-lg-2 col-md-2 col-sm-1">
 		</div>
 	</div>
 
-
+	</div>
 
 	<footer class="text-center">
 		<div class="footer-copyright py-3">
