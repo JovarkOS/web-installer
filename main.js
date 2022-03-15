ea3c31ced11d9a037cc36e6914b9cf2eb13bb7b1
Lucas Burlingham <lucas.burlingham@icloud.com>
2022-03-14 23:11:46 -0400

    Pass options to next location

--- main.js
+++ main.js
@@ -12,27 +12,33 @@ function fade_out(current_step) {
 }
 
 function next(current_step) {
+	// get the GET parameters passed in fromt he URL and add them to the next page
+	// These options can then be passed to be processed with the $_GET array in PHP
+	var url = window.location.href;
+	options = url.substring(url.indexOf("?") + 1);
 	switch (current_step) {
 		case 0:
+			// nothing to do for this step
 			location_next = "/steps/1_disks.php";
 			break;
 		case 1:
-			location_next = "/steps/2_packages.php";
+			// first step with options to pass onto the next pages
+			location_next = "/steps/2_packages.php" + "?" + options;
 			break;
 		case 2:
-			location_next = "/steps/3_configure_system.php";
+			location_next = "/steps/3_configure_system.php" + "?" + options;
 			break;
 		case 3:
-			location_next = "/steps/4_configure_users.php";
+			location_next = "/steps/4_configure_users.php" + "?" + options;
 			break;
 		case 4:
-			location_next = "/steps/5_summary.php";
+			location_next = "/steps/5_summary.php" + "?" + options;
 			break;
 		case 5:
-			location_next = "/steps/6_install.php";
+			location_next = "/steps/6_install.php" + "?" + options;
 			break;
 		default:
-			location_next = "/steps/0_error.php";
+			location_next = "/steps/0_error.php" + "?" + options;
 	}
 
 	setTimeout(function () {
